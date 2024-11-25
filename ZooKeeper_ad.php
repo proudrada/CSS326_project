d59b5bfd7c7eaa26e27c1e7a1c9bb4f30b543ffc
Gray1818dev <lilanadgray1818@gmail.com>
2024-11-25 20:20:15 +0700

    formedit

--- ZooKeeper_ad.php
+++ ZooKeeper_ad.php
@@ -93,7 +93,7 @@ $admin = $result_admin->fetch_assoc();
                     <p>Salary: <span contenteditable='true'>\${$row['Salary']}</span></p>
                     <p>Animal ID(s): <span contenteditable='true'>{$animal_ids_str}</span></p>  <!-- Display Animal IDs -->
                     <p>Age: <span contenteditable='true'>{$age}</span></p>
-                    <a href='edit_zookeep.php?ZK_ID={$row['ZK_ID']}' class='edit-icon' title='Edit'>&#9998;</a>
+                    <a href='from_edit_zookeep.php?ZK_ID={$row['ZK_ID']}' class='edit-icon' title='Edit'>&#9998;</a>
                 </div>";
             }
         } else {
