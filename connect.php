c7923fdec1428d54d0d206ef7a276396d937795c
proudrada <99850042+proudrada@users.noreply.github.com>
2024-11-27 19:42:26 +0700

    zoo

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli('localhost','root','root','zoo(3)');
+$mysqli = new mysqli('localhost','root','root','zoo');
    if($mysqli->connect_errno){
       echo $mysqli->connect_errno.": ".$mysqli->connect_error;
    }
